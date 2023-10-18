5486355d07c01492bb1a3f04e59ff4c07f925b96
Ngoc Thach <dreon1008@gmail.com>
2023-10-18 11:14:30 +0700

    migration

--- src/Application/Shared/Constant/PermissionSlug.cs
+++ src/Application/Shared/Constant/PermissionSlug.cs
@@ -4,8 +4,8 @@ public static class PermissionSlugConst
 {
   public const string MANAGE_USERS = "USR.ALL";
   public const string MANAGE_ROLE = "ROLE.ALL";
-  public const string MANAGE_PERMISSION = "PERMISSION.ALL";
-  public const string MANAGE_CATEGORY = "CATEGORY.ALL";
+  public const string MANAGE_PERMISSION = "PER.ALL";
+  public const string MANAGE_CATEGORY = "CATE.ALL";
   public const string MANAGE_BLOG = "BLOG.ALL";
   public const string MANAGE_OWNED_BLOG = "BLOG.OWN";
   // studio permission
@@ -16,15 +16,15 @@ public static class PermissionSlugConst
   public const string MANAGE_STUDIO_SERVICES = "STU_S.ALL";
   public const string MANAGE_STUDIO_BOOKING = "STU_B.ALL";
   public const string MANAGE_STUDIO_INVOICE = "STU_I.ALL";
-  public const string VIEW_STUDIO_CUSTOMERS = "STU_U.READ";
-  public const string VIEW_STUDIO_ARTISTS = "STU_A.READ";
-  public const string VIEW_STUDIO_ARTIST_SCHEDULE = "STU_AS.READ";
-  public const string VIEW_STUDIO_SERVICES = "STU_S.READ";
-  public const string VIEW_STUDIO_BOOKING = "STU_B.READ";
-  public const string VIEW_STUDIO_INVOICE = "STU_I.READ";
-  public const string VIEW_OWNED_INVOICE = "USR_I.READ";
+  public const string VIEW_STUDIO_CUSTOMERS = "STU_U.R";
+  public const string VIEW_STUDIO_ARTISTS = "STU_A.R";
+  public const string VIEW_STUDIO_ARTIST_SCHEDULE = "STU_AS.R";
+  public const string VIEW_STUDIO_SERVICES = "STU_S.R";
+  public const string VIEW_STUDIO_BOOKING = "STU_B.R";
+  public const string VIEW_STUDIO_INVOICE = "STU_I.R";
+  public const string VIEW_OWNED_INVOICE = "USR_I.R";
 
   // testimonial
-  public const string MANAGE_TESTIMONIAL = "TESTIMONIAL.ALL";
-  public const string MANAGE_OWN_TESTIMONIAL = "TESTIMONIAL.OWN";
+  public const string MANAGE_TESTIMONIAL = "TESTI.ALL";
+  public const string MANAGE_OWN_TESTIMONIAL = "TESTI.OWN";
 }
