9c13a60f70b04cb6eba1141c6b0a5f9dde8b7dbd
GiangNNT <giangnntse173469@fpt.edu.vn>
2023-10-23 10:11:32 +0700

    GetStudioServicePages

--- src/Domain/Studio/IStudioServiceRepository.cs
+++ src/Domain/Studio/IStudioServiceRepository.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using art_tattoo_be.Domain.Studio;
+using art_tattoo_be.src.Application.DTOs.StudioService;
 
 namespace art_tattoo_be.src.Domain.Studio
 {
     public interface IStudioServiceRepository
-    {   
+    {
         IEnumerable<StudioService> GetAll();
         StudioService GetById(Guid id);
         int CreateStudioService(StudioService studioService);
         int DeleteStudioService(Guid id);
+        StudioServiceList GetStudioServicePages(GetStudioServiceQuery req);
     }
 }
--- src/Infrastructure/Repository/StudioServiceRepository.cs
+++ src/Infrastructure/Repository/StudioServiceRepository.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using art_tattoo_be.Domain.Category;
 using art_tattoo_be.Domain.Studio;
 using art_tattoo_be.Infrastructure.Database;
+using art_tattoo_be.src.Application.DTOs.StudioService;
 using art_tattoo_be.src.Domain.Studio;
+using Microsoft.EntityFrameworkCore;
 
 namespace art_tattoo_be.src.Infrastructure.Repository
 {
@@ -19,16 +22,16 @@ namespace art_tattoo_be.src.Infrastructure.Repository
 
     public int CreateStudioService(StudioService studioService)
     {
-        studioService.Id = Guid.NewGuid();
-        _dbContext.StudioServices.Add(studioService);
-        return _dbContext.SaveChanges();
+      studioService.Id = Guid.NewGuid();
+      _dbContext.StudioServices.Add(studioService);
+      return _dbContext.SaveChanges();
     }
 
     public int DeleteStudioService(Guid id)
     {
-        var studioService = _dbContext.StudioServices.Find(id) ?? throw new Exception("Studio Service not found");
-        _dbContext.Remove(studioService);
-        return _dbContext.SaveChanges();
+      var studioService = _dbContext.StudioServices.Find(id) ?? throw new Exception("Studio Service not found");
+      _dbContext.Remove(studioService);
+      return _dbContext.SaveChanges();
     }
 
     public IEnumerable<StudioService> GetAll()
@@ -39,6 +42,40 @@ namespace art_tattoo_be.src.Infrastructure.Repository
     public StudioService GetById(Guid id)
     {
       return _dbContext.StudioServices.Find(id) ?? throw new Exception("Studio Service not found");
+    }
+
+    public StudioServiceList GetStudioServicePages(GetStudioServiceQuery req)
+    {
+      string searchKeyword = req.SearchKeyword ?? "";
+      var query = _dbContext.StudioServices
+      .Where(stuser => stuser.Name.Contains(searchKeyword));
+      int totalCount = query.Count();
+
+      var studioService = query
+      .Include(stuser => stuser.Studio)
+      .Include(stuser => stuser.Category)
+      .Select(stuser => new StudioService
+      {
+        Id = stuser.Id,
+        StudioId = stuser.StudioId,
+        CategoryId = stuser.CategoryId,
+        Name = stuser.Name,
+        Description = stuser.Description,
+        MinPrice = stuser.MinPrice,
+        MaxPrice = stuser.MaxPrice,
+        Discount = stuser.Discount,
+        Studio = stuser.Studio,
+        Category = stuser.Category
+      })
+      .OrderByDescending(stuser => stuser.Name)
+      .Skip(req.Page * req.PageSize)
+      .Take(req.PageSize)
+      .ToList();
+      return new StudioServiceList
+      {
+        StudioServices = studioService,
+        TotalCount = totalCount
+      };
     }
   }
 }
